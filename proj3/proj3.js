c5c4821818057b703de76315bf6c26617d7638ae
DellaCelina <dks0554@gmail.com>
2017-12-03 07:20:44 +0900

    Hierarchical Object

--- proj3/proj3.js
+++ proj3/proj3.js
@@ -82,6 +82,148 @@ function Shader(gl_context){
 
 }
 
+// When Call Draw with Stack that have matrix finished calibration,
+// it multiplies own matrix and use it for draw obj,
+// then it calls child's draw function with stack.
+function HElement(toParent, drawableObj){
+    this.toParent = toParent;
+    this.Obj = drawableObj;
+    this.childs = [];
+    this.childCnt = 0;
+    this.rotate = [0.0, 0.0, 0.0, 1.0];
+    this.scale = [1.0, 1.0, 1.0];
+    this.pushChild = function(HElement, toChild){
+        this.childs.push({toChild : toChild, Child : HElement});
+        this.childCnt++;
+    }
+    this.setRotate = function(angle, x, y, z){
+        this.rotate = [angle, x, y, z];
+    }
+    this.setScale = function(x, y, z){
+        this.scale = [x, y, z];
+    }
+    this.getTranslate = function(scale, joint, type){
+        var mat = new Matrix4();
+        if(type == -1)
+            mat.setTranslate(-joint[0] * scale[0], -joint[1] * scale[1], -joint[2] * scale[2]);
+        else if(type == 1)
+            mat.setTranslate(joint[0] * scale[0], joint[1] * scale[1], joint[2] * scale[2]);
+        return mat;
+    }
+    this.draw = function(gl, shader, stack){
+        stack.setTop(stack.top()
+        .rotate(this.rotate[0], this.rotate[1], this.rotate[2], this.rotate[3])
+        .multiply(this.getTranslate(this.scale, this.toParent, -1)));
+    
+        shader.SetModel(stack.top().scale(this.scale[0], this.scale[1], this.scale[2]));
+        if(!this.Obj.draw(gl)) console.log('Fail to draw Obj');
+
+        for(var i = 0; i<this.childCnt; i++){
+            stack.push(stack.top().multiply(this.getTranslate(this.scale, this.childs[i].toChild, 1)));
+            this.childs[i].Child.draw(gl, shader, stack);
+        }
+        stack.pop();
+    }
+}
+
+function Globe(div, color){
+    this.div = div;
+    this.color = color;
+    this.draw = function(gl){
+        var SPHERE_DIV = this.div;
+        var i, ai, si, ci;
+        var j, aj, sj, cj;
+        var p1, p2;
+
+        var positions = [];
+        var indices = [];
+
+        // Generate coordinates
+        for (j = 0; j <= SPHERE_DIV; j++) {
+            aj = j * Math.PI / SPHERE_DIV;
+            sj = Math.sin(aj);
+            cj = Math.cos(aj);
+            for (i = 0; i <= SPHERE_DIV; i++) {
+                ai = i * 2 * Math.PI / SPHERE_DIV;
+                si = Math.sin(ai);
+                ci = Math.cos(ai);
+
+                positions.push(si * sj);  // X
+                positions.push(cj);       // Y
+                positions.push(ci * sj);  // Z
+            }
+        }
+
+        // Generate indices
+        for (j = 0; j < SPHERE_DIV; j++) {
+            for (i = 0; i < SPHERE_DIV; i++) {
+                p1 = j * (SPHERE_DIV+1) + i;
+                p2 = p1 + (SPHERE_DIV+1);
+
+                indices.push(p1);
+                indices.push(p2);
+                indices.push(p1 + 1);
+
+                indices.push(p1 + 1);
+                indices.push(p2);
+                indices.push(p2 + 1);
+            }
+        }
+
+        // Write the vertex property to buffers (coordinates and normals)
+        // Same data can be used for vertex and normal
+        // In order to make it intelligible, another buffer is prepared separately
+        var a_Position = initArrayBuffer(gl, 'a_Position', new Float32Array(positions), gl.FLOAT, 3);
+        if(a_Position == -1) return false;
+        var a_Normal = initArrayBuffer(gl, 'a_Normal', new Float32Array(positions), gl.FLOAT, 3);
+        if(a_Normal == -1)  return false;
+        var a_Color = gl.getAttribLocation(gl.program, 'a_Color');
+        gl.vertexAttrib4fv(a_Color, this.color);
+
+        // Unbind the buffer object
+        gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+        // Write the indices to the buffer object
+        var indexBuffer = gl.createBuffer();
+        if (!indexBuffer) {
+            console.log('Failed to create the buffer object');
+            return -1;
+        }
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+
+        // Draw the cube
+        gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
+
+        gl.disableVertexAttribArray(a_Position);
+        gl.disableVertexAttribArray(a_Normal);
+        gl.disableVertexAttribArray(a_Color);
+
+        return true;
+    }
+} 
+
+function HObject(gl, shader, root){
+    this.gl = gl;
+    this.shader = shader;
+    this.root = root;
+    this.stack = {
+        topCnt : 0,
+        data : new Array(100),
+        top : function(){return new Matrix4(this.data[this.topCnt]);},
+        setTop : function(data){this.data[this.topCnt] = data;},
+        pop : function(){this.topCnt--;},
+        push : function(data){this.topCnt++; this.setTop(data);},
+        clear : function(){this.topCnt = 0;}
+    };
+    this.draw = function(modelMatrix){
+        this.stack.clear();
+        this.stack.setTop(modelMatrix);
+        //this.root.setScale(0,0,0);
+        this.root.draw(this.gl, this.shader, this.stack);
+    }
+}
+
 function main() {
     // Retrieve <canvas> element
     var canvas = document.getElementById('webgl');
@@ -99,12 +241,6 @@ function main() {
     var frag_src = document.getElementById('fragment-shader').text;
     shader.InitShader(vertex_src, frag_src);
 
-    var n = initVertexBuffers(gl);
-    if (n < 0) {
-        console.log('Failed to set the vertex information');
-    return;
-    }
-
     // Set the clear color and enable the depth test
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.enable(gl.DEPTH_TEST);
@@ -115,14 +251,14 @@ function main() {
     var specularColor = [1.0, 1.0, 1.0];
     var position = [0.0, 1.5, 1.5];
     var lookDirection = [0.0, -1.5, -1.5]
-    var cutoffAngle = 27;
+    var cutoffAngle = 90;
     shader.SetLight(0, diffuseColor, specularColor, position, lookDirection, cutoffAngle);
 
     var diffuseColor = [1.0, 1.0, 1.0];
     var specularColor = [1.0, 1.0, 1.0];
     var position = [0.0, 0.0, 1.5];
     var lookDirection = [0.0, 0.0, -1.0]
-    var cutoffAngle = 30;
+    var cutoffAngle = 90;
     shader.SetLight(1, diffuseColor, specularColor, position, lookDirection, cutoffAngle);
 
     // Set Material
@@ -130,7 +266,7 @@ function main() {
 
     // Set Camera
     var PMatrix = new Matrix4();
-    var lookPoint = [0, 0, 6];
+    var lookPoint = [3, 0, 6];
     var centerPoint = [0, 0, 0];
     var cameraUpVector = [0, 1, 0];
     PMatrix.setPerspective(30, canvas.width/canvas.height, 1, 100);
@@ -138,15 +274,28 @@ function main() {
 
     // Set Model
     var modelMatrix = new Matrix4();  // Model matrix
-    modelMatrix.setRotate(90, 0, 1, 0); // Rotate around the y-axis
-    shader.SetModel(modelMatrix);
+    modelMatrix.setTranslate(0, 0, 0); // Rotate around the y-axis
+
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    // Draw the cube
-    gl.drawElements(gl.TRIANGLES, n, gl.UNSIGNED_SHORT, 0);
+    var root = new HElement([0.0, 0.0, 0.0], new Globe(80, [0.7, 0.3, 0.2, 1.0]));
+    root.setScale(0.5, 0.5, 0.5);
+    root.setRotate(30, 0, 0, 1);
+    var child = new HElement([-1.0, 0.0, 0.0], new Globe(80, [0.3, 0.3, 0.8, 1.0]));
+    child.setScale(0.5, 0.5, 0.5);
+    root.pushChild(child, [1.0, 0.0, 0.0]);
+    var child2 = new HElement([0.0, -1.0, 0.0], new Globe(80, [0.2, 0.3, 0.9, 1.0]));
+    child2.setScale(0.4, 0.4, 0.4);
+    root.pushChild(child2, [0.0, 1.0, 0.0]);
+    var child3 = new HElement([0.0, 0.0, -1.0], new Globe(80, [0.5, 0.3, 0.6, 1.0]));
+    child3.setScale(0.2, 0.2, 0.2);
+    child2.pushChild(child3, [0.0, 0.0, 1.0]);
+    var obj = new HObject(gl, shader, root);
+
+    obj.draw(modelMatrix);
 }
 
-function initVertexBuffers(gl) { // Create a sphere
+function drawCircle(gl){
     var SPHERE_DIV = 80;
     var i, ai, si, ci;
     var j, aj, sj, cj;
@@ -190,8 +339,10 @@ function initVertexBuffers(gl) { // Create a sphere
     // Write the vertex property to buffers (coordinates and normals)
     // Same data can be used for vertex and normal
     // In order to make it intelligible, another buffer is prepared separately
-    if (!initArrayBuffer(gl, 'a_Position', new Float32Array(positions), gl.FLOAT, 3)) return -1;
-    if (!initArrayBuffer(gl, 'a_Normal', new Float32Array(positions), gl.FLOAT, 3))  return -1;
+    var a_Position = initArrayBuffer(gl, 'a_Position', new Float32Array(positions), gl.FLOAT, 3);
+    if(a_Position == -1) return false;
+    var a_Normal = initArrayBuffer(gl, 'a_Normal', new Float32Array(positions), gl.FLOAT, 3);
+    if(a_Normal == -1)  return false;
     var a_Color = gl.getAttribLocation(gl.program, 'a_Color');
     gl.vertexAttrib4f(a_Color, 0.5, 0.3, 1.0, 1.0);
 
@@ -207,7 +358,14 @@ function initVertexBuffers(gl) { // Create a sphere
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
 
-    return indices.length;
+    // Draw the cube
+    gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
+
+    gl.disableVertexAttribArray(a_Position);
+    gl.disableVertexAttribArray(a_Normal);
+    gl.disableVertexAttribArray(a_Color);
+
+    return true;
 }
 
 function initArrayBuffer(gl, attribute, data, type, num) {
@@ -215,7 +373,7 @@ function initArrayBuffer(gl, attribute, data, type, num) {
     var buffer = gl.createBuffer();
     if (!buffer) {
         console.log('Failed to create the buffer object');
-        return false;
+        return -1;
     }
     // Write date into the buffer object
     gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
@@ -224,11 +382,11 @@ function initArrayBuffer(gl, attribute, data, type, num) {
     var a_attribute = gl.getAttribLocation(gl.program, attribute);
     if (a_attribute < 0) {
         console.log('Failed to get the storage location of ' + attribute);
-        return false;
+        return -1;
     }
     gl.vertexAttribPointer(a_attribute, num, type, false, 0, 0);
     // Enable the assignment of the buffer object to the attribute variable
     gl.enableVertexAttribArray(a_attribute);
 
-    return true;
+    return a_attribute;
 }
